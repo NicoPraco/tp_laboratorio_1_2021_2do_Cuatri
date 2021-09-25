4ef593ad5f691753a9d28d9e984fe2a70c517830
NicoPraco <kualon34@gmail.com>
2021-09-25 17:05:43 -0300

    Trabajo Practico Nº1

---
+++ TP_1/src/input.h
@@ -0,0 +1,84 @@
+/**
+ * brief Limpia la consola.
+ */
+void clearScreen();
+
+/**
+* \brief Solicita un numero al usuario y lo valida
+* \param input Se carga el numero ingresado
+* \param message Es el mensaje a ser mostrado
+* \param eMessage Es el mensaje a ser mostrado en caso de error
+* \param lowLimit Limite inferior a validar
+* \param hiLimit Limite superior a validar
+* \return Si obtuvo el numero [0] si no [-1]
+*
+*/
+int getInt(int* input,char message[],char eMessage[], int lowLimit, int hiLimit);
+
+/**
+* \brief Solicita un numero al usuario y lo valida
+* \param input Se carga el numero ingresado
+* \param message Es el mensaje a ser mostrado
+* \param eMessage Es el mensaje a ser mostrado en caso de error
+* \param lowLimit Limite inferior a validar
+* \param hiLimit Limite superior a validar
+* \return Si obtuvo el numero [0] si no [-1]
+*
+*/
+int getFloat(float* input,char message[],char eMessage[], float lowLimit, float hiLimit);
+
+/**
+* \brief Solicita un caracter al usuario y lo valida
+* \param input Se carga el caracter ingresado
+* \param message Es el mensaje a ser mostrado
+* \param eMessage Es el mensaje a ser mostrado en caso de error
+* \param lowLimit Limite inferior a validar
+* \param hiLimit Limite superior a validar
+* \return Si obtuvo el caracter [0] si no [-1]
+*
+*/
+int getChar(char* input,char message[],char eMessage[], char lowLimit, char hiLimit);
+
+/**
+* \brief Solicita una cadena de caracteres al usuario y la valida
+* \param input Se carga el string ingresado
+* \param message Es el mensaje a ser mostrado
+* \param eMessage Es el mensaje a ser mostrado en caso de error
+* \param lowLimit Longitud minima de la cadena
+* \param hiLimit Longitud maxima de la cadena
+* \return Si obtuvo la cadena [0] si no [-1]
+*
+*/
+int getString(char* input,char message[],char eMessage[], int lowLimit, int hiLimit);
+
+////////////////////////////////////////////////////////////////////////////////////
+
+/**
+* \brief Solicita un numero al usuario y no lo valida.
+* \param input Se carga el numero ingresado
+* \param message Es el mensaje a ser mostrado
+* \return Si obtuvo el numero [0] si no [-1]
+*
+*/
+int getIntNoVal(int* input, char message[]);
+
+/**
+* \brief Solicita un numero al usuario y no lo valida
+* \param input Se carga el numero ingresado
+* \param message Es el mensaje a ser mostrado
+* \return Si obtuvo el numero [0] si no [-1]
+*
+*/
+int getFloatNoVal(float* input,char message[]);
+
+////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * \brief Verifica que el numero ingresado sea un entero.
+ * \param num float. Numero a ser verificado.
+ * \return Si obtuvo el numero 1 el numero es ENTERO, si no 0.
+ */
+int isNumInt(float num);
+
+
+
